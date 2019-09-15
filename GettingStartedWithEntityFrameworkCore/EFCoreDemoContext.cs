12f46a258395c1278b66b9753548ec514a13ae0c
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-15 18:51:02 +0200

    Install dependencies add models

---
+++ GettingStartedWithEntityFrameworkCore/EFCoreDemoContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GettingStartedWithEntityFrameworkCore
+{
+    public class EFCoreDemoContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\;Database=EFCoreDemo;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
