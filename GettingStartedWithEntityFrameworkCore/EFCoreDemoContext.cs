16d63e289b22e63c4e2c6cb8429b42b422572d06
Sebastian Karasiewicz <sekarasiewicz@gmail.com>
2019-09-15 22:24:20 +0200

    connect to sql

--- GettingStartedWithEntityFrameworkCore/EFCoreDemoContext.cs
+++ GettingStartedWithEntityFrameworkCore/EFCoreDemoContext.cs
@@ -8,7 +8,7 @@ namespace GettingStartedWithEntityFrameworkCore
         public DbSet<Author> Authors { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=.\;Database=EFCoreDemo;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer(@"Server=127.0.0.1,1433;Database=EFCoreDemo;Integrated Security=False;User Id=sa;Password=Your_password123;MultipleActiveResultSets=True");
         }
     }
 }
